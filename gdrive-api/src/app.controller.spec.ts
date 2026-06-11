b309c91db2d576518ab708aec8223199d7e7d3fe
MagnunAVF <magnunavf@gmail.com>
2026-06-11 00:49:44 -0300

    chore: setup config and env vars

--- gdrive-api/src/app.controller.spec.ts
+++ gdrive-api/src/app.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigService } from '@nestjs/config';
 
 describe('AppController', () => {
   let appController: AppController;
@@ -8,15 +9,35 @@ describe('AppController', () => {
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [
+        AppService,
+        {
+          provide: ConfigService,
+          useValue: {
+            get: jest.fn((key: string) => {
+              if (key === 'PORT') {
+                return 3000;
+              }
+              if (key === 'DATABASE_HOST') {
+                return 'localhost';
+              }
+              return null;
+            }),
+          },
+        },
+      ],
     }).compile();
 
     appController = app.get<AppController>(AppController);
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return a JSON object with environment details', () => {
+      expect(appController.getHello()).toEqual({
+        environment: 'test',
+        port: 3000,
+        databaseHost: 'localhost',
+      });
     });
   });
 });
